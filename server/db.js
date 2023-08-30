540052e54ef9e07cc99d202a2710d8652a0c376d
Hyung Geun Lee <hyunggeunlee@Hyungui-MacBookPro.local>
2023-08-30 14:00:45 +0900

    Run Application with Docker

--- server/db.js
+++ server/db.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+const conection_uri = () => {
+    return `mongodb://${process.env.MONGODB_URI}:27017/MERN`;
+};
+
 module.exports = () => {
-	const connection = mongoose
-		.connect(process.env.MONGODB_URI)
-		.then((result) => console.log("Connected to database"))
-		.catch((err) => console.log("could not connect to database"));
+    const connection = mongoose
+        .connect(conection_uri())
+        .then((result) => console.log("Connected to database"))
+        .catch((err) => {
+            console.log(err);
+            console.log("could not connect to database");
+        });
 };
